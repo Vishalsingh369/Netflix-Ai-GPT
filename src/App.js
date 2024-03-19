539c22417cf2d00c0fd2262976752432ad6d3514
Vishal Thakur <vishalthakur0378958@gmail.com>
2024-03-19 20:16:39 +0530

    initialize craand tailwind

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-500">
+      <h1 className="text-center mt-24">Welcome to React</h1>
+      <h2 className="text-center mt-34">Tailwind CSS</h2>
+    </div>
+  );
+}
+
+export default App;

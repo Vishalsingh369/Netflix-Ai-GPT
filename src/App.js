6c80cccddb9be0acd5ef5092798b19328ba2e13e
Vishal Thakur <vishalthakur0378958@gmail.com>
2024-03-26 12:48:38 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-500">
-      <h1 className="text-center mt-24">Welcome to React</h1>
-      <h2 className="text-center mt-34">Tailwind CSS</h2>
+    <div>
+      <h1>App</h1>
+      <Body />
+      
     </div>
   );
 }
